5be106b87c2d8fcde643ecde0da76cd8f49b4661
NLSanyu <lydiansanyu@gmail.com>
2018-11-01 14:58:13 +0200

    Add table to parcels page

--- UI/send_it_script.js
+++ UI/send_it_script.js
@@ -1,21 +1,22 @@
-#Parcels page
+var destination, order_canceled;
+//var create = '';
+//var view = '';
+
 function createDelivery(){
-	alert("Create delivery");
-	document.getElementById('parcel-details').innerHTML = <p>Create parcel order</p>;
+	document.getElementById('parcel-details').innerHTML = '<p>Create parcel order</p>';
 }
 
 function viewDeliveries(){
-	alert("Create delivery");
-	document.getElementById('parcel-details').innerHTML = <p>Parcel order deliveries</p>;
+	document.getElementById('parcel-details').innerHTML = '<p>Parcel order deliveries</p>';
 }
 
 function changeDestination(){
-	alert("Change destination");
-	return 1;
+	destination = prompt("Change destination");
 }
 
 function cancelOrder(){
-	alert("Cancel order");
-	return 1;
+	if(status != "delivered"){
+		order_canceled = alert("Cancel order");
+	}
 }
 
