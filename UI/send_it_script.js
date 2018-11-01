7312df0869e95a822adb59ea3b3ebd294d6287d5
NLSanyu <lydiansanyu@gmail.com>
2018-11-01 13:56:22 +0200

    Add admin sign in and admin page

--- UI/send_it_script.js
+++ UI/send_it_script.js
@@ -1,8 +1,21 @@
-function changeDest(){
+#Parcels page
+function createDelivery(){
+	alert("Create delivery");
+	document.getElementById('parcel-details').innerHTML = <p>Create parcel order</p>;
+}
+
+function viewDeliveries(){
+	alert("Create delivery");
+	document.getElementById('parcel-details').innerHTML = <p>Parcel order deliveries</p>;
+}
+
+function changeDestination(){
 	alert("Change destination");
 	return 1;
 }
 
 function cancelOrder(){
-	return 2;
+	alert("Cancel order");
+	return 1;
 }
+
