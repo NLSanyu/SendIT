9397cb4dc28f6ac3759f2a070fc2f6bda596e0ba
NLSanyu <lydiansanyu@gmail.com>
2018-11-02 13:04:26 +0200

    Add functionality to Javascript file

--- UI/send_it_script.js
+++ UI/send_it_script.js
@@ -1,17 +1,10 @@
-var destination, order_canceled;
-//var create = '';
-//var view = '';
-
-function createDelivery(){
-	document.getElementById('parcel-details').innerHTML = '<p>Create parcel order</p>';
-}
-
-function viewDeliveries(){
-	document.getElementById('parcel-details').innerHTML = '<p>Parcel order deliveries</p>';
-}
+var dest, order_canceled;
 
 function changeDestination(){
-	destination = prompt("Change destination");
+	document.getElementById('dest').innerHTML = '<form> <textarea name="parcel_details" rows="3"> </textarea>';
+	dest = document.getElementById('button-dest');
+	dest.innerHTML = '<input type="submit" name="new_dest" value="Confirm destination change" class="submit-button"> </input> </form>';
+	//dest.class="submit-button";
 }
 
 function cancelOrder(){
@@ -19,4 +12,13 @@ function cancelOrder(){
 		order_canceled = alert("Cancel order");
 	}
 }
+
+function adminViewUsers(){
+	document.getElementById('parcel-details').innerHTML = '<p>SendIT Users</p>';
+}
+
+function adminViewParcels(){
+	document.getElementById('parcel-details').innerHTML = '<p>Parcel order deliveries</p>';
+}
+
 
