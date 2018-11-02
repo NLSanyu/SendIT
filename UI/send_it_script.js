fb5333eb94201ff7f6eadfd09774098fc469ab56
NLSanyu <lydiansanyu@gmail.com>
2018-11-02 18:50:24 +0200

    Change background pattern Add to JS file

--- UI/send_it_script.js
+++ UI/send_it_script.js
@@ -1,16 +1,25 @@
-var dest, order_canceled;
+var dest, p, node;
+
+function showTaglines(){
+	var tags = ["abc", "def"];
+	var tag_area = document.getElementByClassName('home-tagline');
+	for(i=0;i<tags.length;i++){
+		p = document.createElement("p");
+        node = document.createTextNode(tags[i]);
+		p.appendChild(node);
+		tag_area.appendChild(p);
+	}
+}
 
 function changeDestination(){
 	document.getElementById('dest').innerHTML = '<form> <textarea name="parcel_details" rows="3"> </textarea>';
 	dest = document.getElementById('button-dest');
-	dest.innerHTML = '<input type="submit" name="new_dest" value="Confirm destination change" class="submit-button"> </input> </form>';
+	dest.innerHTML = '<input type="submit" name="new_dest" value="Confirm destination change" class="submit-button"> </form>';
 	//dest.class="submit-button";
 }
 
 function cancelOrder(){
-	if(status != "delivered"){
-		order_canceled = alert("Cancel order");
-	}
+	alert("Order cancelled");
 }
 
 function adminViewUsers(){
