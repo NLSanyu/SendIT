e5e1d8f4e86df7f5c3f409cb100bf28d0c9a68c0
NLSanyu <lydiansanyu@gmail.com>
2018-11-04 12:47:21 +0200

    Edit JS file

--- UI/send_it_script.js
+++ UI/send_it_script.js
@@ -1,18 +1,5 @@
-var dest, p, node, tag_area;
-
-function showTaglines(){
-	var tags = ["A courier service you can trust", "Deliver parcels to different destinations", "Deliver parcels to different destinations", "Keep track of your deliveries", "Benefit from the best prices on the market"];
-	for(i=0;i<tags.length;i++){
-		setTimeout(function taglines(){
-			tag_area = document.getElementById("tag");
-			p = document.createElement("p");
-			//node = document.createTextNode(tags[i]);
-		    node = document.createTextNode("abc");
-			p.appendChild(node);
-			tag_area.appendChild(p);
-		}, 2000);
-	}
-}
+var dest, signedIn=false
+var sign = "Sign In"
 
 function changeDestination(){
 	document.getElementById('dest').innerHTML = '<form> <textarea name="parcel_details" rows="3"> </textarea>';
@@ -28,16 +15,27 @@ function changeStatus(){
 	//dest.class="submit-button";
 }
 
-function cancelOrder(){
-	alert("Order cancelled");
+function signIn(){
+	signedIn = true;
+	alert("Hey")
 }
 
-function validateParcelForm(){
-	var val = document.forms["create_parcel_form"]["parcel_desc"].value;
-    if (val == "") {
-        alert("Description must be filled out");
-        return false;
-    }
+function signOut(){
+	signedIn = false;
+}
+
+function showSignInOrOut(){
+	var el = document.getElementById('sign'); 
+	if(signedIn == true){
+		el.innerHTML = "Sign Out";
+	}
+	else {
+		el.innerHTML = "Sign In";
+	}
+}
+
+function cancelOrder(){
+	alert("Order cancelled");
 }
 
 function adminViewUsers(){
