bde15a7f427e30f2c34f174167985b64efd16188
NLSanyu <lydiansanyu@gmail.com>
2018-11-30 14:09:45 +0200

    Connect parcels page to api

--- UI/static/parcels.js
+++ UI/static/parcels.js
@@ -16,7 +16,39 @@ function createParcel(){
     .then((data) => console.log(data))
     .catch((err) => console.log(err)) 
   
-  }
+}
+
+function getParcels(){
+    auth = `Bearer ` + localStorage.getItem("access_token");
+
+    fetch('http://127.0.0.1:5000/api/v1/parcels', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': auth
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        let output = "";
+        data.forEach(function(parcel){
+            output += `
+                <tr>
+                    <td>${parcel.description}</td>
+                    <td>${parcel.pickup_location}</td>
+                    <td contenteditable="true">${parcel.destination}</td>
+                    <td>${parcel.price}</td>
+                    <td>${parcel.status}</td>
+                    <td><i class="fas fa-times"></i></td>
+                </tr>
+            `
+        })
+    })
+    .catch((err) => console.log(err)) 
+}
 
 
-  //'Authorization': `Bearer ${localStorage.getItem("access_token")}`
+
+
+
+//'Authorization': `Bearer ${localStorage.getItem("access_token")}`
