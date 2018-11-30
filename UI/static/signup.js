df5d38ce12a6e6bf0603121f9ec64cfb6f50ec9c
NLSanyu <lydiansanyu@gmail.com>
2018-11-30 11:10:24 +0200

    Connect signup to api

---
+++ UI/static/signup.js
@@ -0,0 +1,21 @@
+function signUp(){
+  let username = document.getElementById("uname1").value;
+  let email = document.getElementById("email1").value;
+  let phoneNumber = document.getElementById("phone1").value;
+  let password = document.getElementById("password1").value;
+  console.log(78);
+
+  fetch('http://127.0.0.1:5000/api/v1/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    }, 
+    body: JSON.stringify({username: username, email: email, phone_number: phoneNumber, password: password})
+  })
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+
+}
+
+
+
