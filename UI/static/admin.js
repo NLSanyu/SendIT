99b2b70f50ab1d8aab1143ece7fdd391e82a1826
NLSanyu <lydiansanyu@gmail.com>
2018-12-02 11:44:03 +0200

    Add admin connection

--- UI/static/admin.js
+++ UI/static/admin.js
@@ -1,27 +1,7 @@
-function createParcel(){
-    let description = document.getElementById("desc").value;
-    let pickupLocation = document.getElementById("pickup").value;
-    let destination = document.getElementById("dest").value;
+function getAllParcels(){
     auth = `Bearer ` + localStorage.getItem("access_token");
-  
+
     fetch('http://127.0.0.1:5000/api/v1/parcels', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-        'Authorization': auth
-      }, 
-      body: JSON.stringify({description: description, pickup_location: pickupLocation, destination: destination})
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err)) 
-  
-}
-
-function getUserParcels(){
-    auth = `Bearer ` + localStorage.getItem("access_token");
-
-    fetch('http://127.0.0.1:5000/api/v1/users/1/parcels', {
       method: 'GET',
       headers: {
         'Content-type': 'application/json',
