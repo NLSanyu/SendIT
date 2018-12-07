1620131b039d2c450380d238f3140ae87b9b2201
NLSanyu <lydiansanyu@gmail.com>
2018-12-07 17:36:09 +0200

    Show user updates using modals

--- UI/static/profile.js
+++ UI/static/profile.js
@@ -18,7 +18,10 @@ function createParcel(){
       body: JSON.stringify({description: description, pickup_location: pickupLocation, destination: destination})
     })
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        console.log(data);
+        showModal(data['message']);
+    })
     .catch((err) => console.log(err)) 
   
 }
@@ -217,6 +220,31 @@ function decodeToken(token){
     console.log(playload);
     
 };
+
+function showModal(info){
+    let modal = document.getElementById('myModal');
+    let modalBody = document.getElementById('modal-body');
+    modal.style.display = "block";
+    modalBody.innerHTML = info;
+
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+  
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+        modal.style.display = "none";
+        }
+    }
+}
+
+
 
 
 
